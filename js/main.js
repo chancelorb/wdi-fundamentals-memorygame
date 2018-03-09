8365ef151d41c086a914b1204981583eb7f80bfa
Chance Baars <chance@Chance.home>
2018-03-09 17:00:34 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardTree = "king";
-var cardFour = "king";
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInplay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInplay.push(cardOne);
+cardsInplay.push(cardTwo);
 
 console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+
+if (cardsInplay.length === 2) {
+  if (cardsInplay[0] === cardsInplay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+};
